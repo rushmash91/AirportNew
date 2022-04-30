bcbd0843911d7cdcfcd53ca1c75239048bfc08fb
Arush Sharma <arushsharma91@gmail.com>
2022-04-30 03:40:26 -0500

    heroku

--- AirportNew/settings.py
+++ AirportNew/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+   BASE_DIR / "mainApp" / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
